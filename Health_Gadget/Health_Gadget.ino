abacdccb0b9f33c4466f11a78143a1d1020ea2c6
amirhaf76 <amir.haf76@gmail.com>
2022-03-09 10:13:35 +0330

    Collecting all module is almost done.

--- Health_Gadget/Health_Gadget.ino
+++ Health_Gadget/Health_Gadget.ino
@@ -1,15 +1,77 @@
+/** 
+ * MAX30205 as body temperature:
+ * Module 	| Arduino Board
+ * vcc 		| 5v or 3.3 v
+ * GND 		| ground
+ * SDA		| A4 or SDA
+ * SCL		| A5 or SCL
+ * 
+ * MAX30102 as Oxi meter:
+ * Module 	| Arduino Board
+ * vcc 		| 3.3 v
+ * GND 		| ground
+ * SDA		| SDA or A4
+ * SCL		| SCL or A5
+ * 
+ * ADXL325 as Accelerator Meter:
+ * Module 	| Arduino Board
+ * vcc 		| 3.3 v
+ * GND 		| ground
+ * x		| A1
+ * y		| A2
+ * z		| A3
+ * 
+ * KY-039 as Heartbeat Radio Sensor:
+ * Module 	| Arduino Board
+ * vcc 		| 5 v
+ * GND 		| ground
+ * output	| A0
+ * 
+ * AD8232 as Echocardiography:
+ * Module 	| Arduino Board
+ * vcc 		| 3.3 v
+ * GND 		| ground
+ * output	| A0
+ * LO+		| 2
+ * LO-		| 3
+ * SDN		| 4
+ * 
+ * SD Module:
+ * Module 	| Arduino Board
+ * vcc 		| 3.3 v
+ * GND 		| ground
+ * CS		| 13
+ * MOSI		| MOSI = 12
+ * MISO		| MISO = 11
+ * SLK		| 10
+ * 
+ * Transistor Controller:
+ * ...
+ * 
+*/
+
+
+
 #include <Wire.h>
 #include "Protocentral_MAX30205.h"
+#include "MAX30105.h"
 #include "health_helper.h"
+#include <SPI.h>
+#include <SD.h>
+
 
 /* Gadget ID*/
 #define ID_GADGET 0xf2
+#define FILE_RECORDS "records"
 
 // It is for debugging program.
 #define DEBUG 1 
 
+
+/* Declaration Part */
+
 // Setup Functions 
-//void MAX30102_setup(); 
+void MAX30102_setup(); // ok
 void MAX30205_setup(); // ok
 void KY_039_setup(); // ok
 void ADXL_325_setup(); // ok
@@ -17,7 +79,7 @@ void ADXL_325_setup(); // ok
 void AD8232_setup(); // ok
 
 // Loop Step Functions
-//void MAX30102_loop_step();
+void MAX30102_loop_step();  // ok
 void MAX30205_loop_step(); // ok
 void KY_039_loop_step(); // ok
 void ADXL_325_loop_step(); // ok
@@ -25,18 +87,13 @@ void ADXL_325_loop_step(); // ok
 void AD8232_loop_step(); // ok
 
 
+/* Global Variables */
+
+// MAX30102
+MAX30105 particleSensor; // initialize MAX30102 with I2C
+// MAX30205
 MAX30205 tempSensor;
 enum Controller c = None;
-
-// ADXL_325
-Point p;
-const int xpin = A0;                  // x-axis of the accelerometer
-const int ypin = A1;                  // y-axis
-const int zpin = A2;                  // z-axis (only on 3-axis models)
-const int CF = 1;
-// AD8232
-const int LO_PLUS = 10;
-const int LO_NEG = 11;
 // KY-039
 const int NUM_POINTs = 50;
 double data[NUM_POINTs];
@@ -46,9 +103,18 @@ int beat;
 int brt;
 unsigned long temp_time;
 int KY_INPUT = A0;
+// ADXL_325
+Point p;
+const int xpin = A0;                  // x-axis of the accelerometer
+const int ypin = A1;                  // y-axis
+const int zpin = A2;                  // z-axis (only on 3-axis models)
+const int CF = 1;
+// AD8232
+const int LO_PLUS = 10;
+const int LO_NEG = 11;
 
 
-// Stepup Declarations
+/* Stepup Declarations */
 void ADXL_325_setup() {
   p.x = analogRead(xpin);
   p.y = analogRead(ypin);
@@ -61,8 +127,8 @@ void MAX30205_setup() {
 
   //scan for temperature in every 30 sec untill a sensor is found. Scan for both addresses 0x48 and 0x49
   while(!tempSensor.scanAvailableSensors()){
-    Serial.println("Couldn't find the temperature sensor, please connect the sensor." );
-    delay(30000);
+ 	Serial.println("Couldn't find the temperature sensor, please connect the sensor." );
+	delay(30000);
   }
 
   tempSensor.begin();   // set continuos mode, active mode
@@ -82,8 +148,33 @@ void KY_039_setup() {
   mean = analogRead(A0);
 }
 
+void MAX30102_setup() {
+  // Serial.begin(115200);
+  while(!Serial); //We must wait for Teensy to come online
+  delay(100);
+  Serial.println("");
+  Serial.println("MAX30102");
+  Serial.println("");
+  delay(100);
+  // Initialize sensor
+  if (particleSensor.begin(Wire, I2C_SPEED_FAST) == false) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
 
-// Loop Step Declarations
+  byte ledBrightness = 70; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 1; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  int sampleRate = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 69; //Options: 69, 118, 215, 411
+  int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+}
+
+
+/* Loop Step Declarations */
 void ADXL_325_loop_step() {
 
   Point temp;
@@ -124,7 +215,7 @@ void AD8232_loop_step() {
   Serial.println(analogRead(A0));
   }
 
-  delay(1)
+  delay(1);
 }
 
 void KY_039_loop_step() {
@@ -175,21 +266,38 @@ void KY_039_loop_step() {
   delay(100);// wait for a second
 }
 
+void MAX30102_loop_step() {
+  particleSensor.check(); //Check the sensor
+  while (particleSensor.available()) {
+      // read stored IR
+      Serial.print(particleSensor.getFIFOIR());
+      Serial.print(",");
+      // read stored red
+      Serial.println(particleSensor.getFIFORed());
+      // read next set of samples
+      particleSensor.nextSample();      
+  }
+}
+
+
 // change 
 void change(enum Controller c) {
   
 }
 // Setup
 void setup() {
-  Serial.begin(9600);
-  pinMode(A0, INPUT); //? ky-039
+	Serial.begin(9600);
+	// pinMode(A0, INPUT); //? ky-039
 
-//  MAX30102_setup();
-  MAX30205_setup(); 
-  KY_039_setup();
-  ADXL_325_setup(); 
-//  SD_setup();
-  AD8232_setup();
+	#if !DEBUG
+	MAX30102_setup();
+	MAX30205_setup(); 
+	KY_039_setup();
+	ADXL_325_setup(); //
+	//  SD_setup();
+	AD8232_setup();
+	#endif
+
 }
 
 
@@ -209,7 +317,7 @@ void loop() {
       #if DEBUG
         Serial.print("MAX30102_");
       #else
-//      MAX30102_loop_step();
+        MAX30102_loop_step();
       #endif
       break;
     case MAX30205_:
@@ -255,5 +363,81 @@ void loop() {
       break;
     default:
       Serial.print("Error. It needs to see codes.");
+  }
+}
+
+bool make_file_ready() {
+  #if DEBUG
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.print("Initializing SD card...");
+  #endif
+
+  if (!SD.begin(4)) {
+    #if DEBUG
+    Serial.println("initialization failed!");
+    #endif
+    return false;
+  }
+  #if DEBUG
+  Serial.println("initialization done.");
+  #endif
+
+  return true;
+}
+
+bool write_in_file(int len, char buff[]) {
+  // making file ready
+  if (!make_file_ready()) 
+    return false; // file is not ready and return
+
+  File myFile = SD.open(FILE_RECORDS, FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    #if DEBUG
+    Serial.print("Writing to test.txt...");
+    #endif
+    
+    myFile.write(buff, len);
+
+    // close the file:
+    myFile.close();
+
+    #if DEBUG
+    Serial.println("done.");
+    #endif
+  } else {
+    // if the file didn't open, print an error:
+    #if DEBUG
+    Serial.println("error opening test.txt");
+    #endif
+  }
+}
+
+void read_from_file(int len, char buff[]) {
+  // re-open the file for reading:
+  File myFile = SD.open(FILE_RECORDS);
+
+  if (myFile) {
+    #if DEBUG
+    Serial.println(FILE_RECORDS);
+    #endif
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    #if DEBUG
+    Serial.println("error opening test.txt");
+    #endif
   }
 }
